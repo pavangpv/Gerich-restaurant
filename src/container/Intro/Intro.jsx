8fa6a47d7a3151b833faa2de4405d3f32442bb7a
pavangpv <pavandhanu20@gmail.com>
2022-12-11 18:04:27 +0530

    video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  const[playVideo,setPlayVideo] = useState(false);
+  const vidRef = useRef();
+return(
+  <div className='app__video'>
+  <video 
+  src={meal}
+  ref={vidRef}
+  type="video/mp4"
+  loop
+  controls={false}
+  muted
+  />
   </div>
-);
-
+)}
 export default Intro;
